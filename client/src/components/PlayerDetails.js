a114632b9bf6a36eede90415dcb548fc7aecb3cb
Temitope-Akinsoto <takinsoto@gmail.com>
2019-10-04 02:27:32 -0700

    feat: Make axios call to fetch list of players
    In this commit,I made an axios call and retrieved the list of player from the server. The result of the
    api call gives an array containin individual player information

--- client/src/components/PlayerDetails.js
+++ client/src/components/PlayerDetails.js
@@ -1,11 +1,8 @@
 import React, {Component} from "react";
 
 export default class PlayerDetails extends Component {
-    constructor(){
-        super();
-    }
 
-    render(){
+    render(){      
         return(
             <div>
                 <h5>This is the player details component</h5>
--- client/src/components/PlayersList.js
+++ client/src/components/PlayersList.js
@@ -1,16 +1,29 @@
 import React, {Component} from "react";
 import PlayerDetails from './PlayerDetails';
+import axios from  "axios";
 
 export default class PlayersList extends Component {
     constructor(){
         super();
+
+        this.state= {
+            playerList: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/players")
+        .then(response => {
+        this.setState({ playerList: response.data })
+        }).catch(error => console.log(error.data));
     }
 
     render(){
         return(
             <div>
                 <h3>This is the player list component</h3>
-                <PlayerDetails />
+                <PlayerDetails />               
+                
             </div>
         )
     }
